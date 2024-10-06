02736f6bbd16f65cdc191ea3b912a9d53d19e409
nguyendz21012003 <66507352+nguyn21012003@users.noreply.github.com>
2024-10-06 09:05:17 +0700

    update finitewell.py

--- quantum well/finitewell.py
+++ quantum well/finitewell.py
@@ -1,9 +1,21 @@
 import numpy as np
-from numpy import tan, sqrt
+from numpy import sqrt, sin, cos, tan
 from math import pi
+import matplotlib.pyplot as plt
+from math import exp
 
 
-def function(z, z0, N):
+def z0_cal():
+    hbar = 1.05457182e-34
+    m = 9.11e-31
+    a = 0.5e-9
+    V0 = 25 * 1.61e-19
+    z0 = a / hbar * sqrt(2 * m * V0)
+    return z0
+
+
+def f(z):
+    z0 = z0_cal()
     f = tan(z) - sqrt((z0 / z) ** 2 - 1)
     return f
 
@@ -46,14 +58,85 @@ def bisection(f, a, b, N, eps):
     return (na, nb, nc), count
 
 
+def df_newton(x):
+    return exp(x) - 4
+
+
+def newtonr(f, df, p0, N, eps):
+    p = np.zeros(N)
+    p[0] = p0
+    count = 0
+    listp = []
+    for i in range(1, N + 1):
+        if i + 1 < N:
+            p[i] = p[i - 1] - f(p[i - 1]) / df(p[i - 1])
+            count += 1
+            if abs(p[i] - p[i - 1]) <= eps:
+                break
+        else:
+            print(f"Nghiem khong hoi tu voi {count} vong lap !")
+            break
+
+    # for i in range(len(p)):
+    #    if p[i] != 0:
+    #        listp.append(float(p[i]))
+
+    return p, count
+
+
+def df_secant(f, pn1, pn2):
+    return (f(pn1) - f(pn2)) / (pn1 - pn2)
+
+
+def secant(f, df, p0, p1, N, eps):
+    p = np.zeros(N)
+    p[0] = p0
+    p[1] = p1
+    count = 0
+    listp = []
+    for i in range(2, N + 2):
+        if i + 1 < N:
+            p[i] = p[i - 1] - f(p[i - 1]) / df(f, p[i - 1], p[i - 2])
+            count += 1
+            if abs(p[i] - p[i - 1]) <= eps:
+                break
+        else:
+            print(f"Nghiem khong hoi tu voi {count} vong lap !")
+            break
+
+    # for i in range(len(p)):
+    #    if p[i] != 0:
+    #        listp.append(float(p[i]))
+
+    return p, count
+
+
+def save_log(arg):
+    pass
+
+
+def plot_data(arg):
+    pass
+
+
 def main():
     N = 100
-    hbar = 1.05457182e-34
-    m = 9.31e-31
-    V0 = 32 * hbar**2 / (m * a**2)
-    a = 1
-    z0 = a / hbar * sqrt(2 * m * V0)
-    function(z0, hbar, N)
+    x0 = 4
+    x1 = 5
+    eps = 10e-15
+
+    p0 = x0
+    p1 = x1
+
+    sol_bisection, count1 = bisection(f, x0, x1, N, eps)
+    na, nb, nc = sol_bisection
+    print(nc, "\n")
+
+    sol_newton, count2 = newtonr(f, df_newton, p0, N, eps)
+    print(sol_newton, "\n")
+
+    sol_secant, count3 = secant(f, df_secant, p0, p1, N, eps)
+    print(sol_secant, "\n")
 
 
 if __name__ == "__main__":
