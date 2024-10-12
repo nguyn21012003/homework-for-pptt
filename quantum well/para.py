588787108feab112c880285f5128f6fc1da38157
nguyendz21012003 <66507352+nguyn21012003@users.noreply.github.com>
2024-10-12 23:06:01 +0700

    update parameters

--- quantum well/para.py
+++ quantum well/para.py
@@ -1,6 +1,6 @@
 from numpy import sqrt
 
-a0 = 5.29e-11  ## Hang so Bohr
+a0 = 1e-9
 m = 9.31e-31  # kg
 Ce = 1.6e-19  # eV to Joule(J)
 hbar_ev = 6.582119569e-16  # eV*s
@@ -10,18 +10,23 @@ hbar_Js = 1.054571817e-34  # J*s
 
 def case_(_):
     match _:
-        case 1:  # z0 ~ 3.468005224411644
-            V0 = 100
-            a = 50 * a0
-            z0 = sqrt(0.064 * m * V0 * a * a / (2 * (hbar_ev) ** 2 * Ce))
-            return a, m, Ce, V0, z0, hbar_ev, hbar_Js
-        case 2:  # z0 ~ 109.6679544650417
-            V0 = 40
-            a = 50 * a0
-            z0 = sqrt(0.064 * m * V0 * a * a / (2 * (hbar_ev) ** 2 * Ce))
-            return a, m, Ce, V0, z0, hbar_ev, hbar_Js
-        case 3:  # z0 ~ 109.6679544650417
+        case 1:  # z0 ~ 6.707666865904062
             V0 = 1
-            a = 15 * a0
-            z0 = sqrt(0.064 * m * V0 * a * a / (2 * (hbar_ev) ** 2 * Ce))
+            a = 10 * a0
+            z0 = sqrt(0.067 * m * V0 * a * a / (2 * (hbar_ev) ** 2 * Ce))
+            return a, m, Ce, V0, z0, hbar_ev, hbar_Js
+        case 2:  # z0 ~ 12.779821793191344
+            V0 = 3
+            a = 11 * a0
+            z0 = sqrt(0.067 * m * V0 * a * a / (2 * (hbar_ev) ** 2 * Ce))
+            return a, m, Ce, V0, z0, hbar_ev, hbar_Js
+        case 3:  # z0 ~ 26.830667463616248
+            V0 = 4
+            a = 20 * a0
+            z0 = sqrt(0.067 * m * V0 * a * a / (2 * (hbar_ev) ** 2 * Ce))
+            return a, m, Ce, V0, z0, hbar_ev, hbar_Js
+        case 4:  # z0 ~ 26.830667463616248
+            V0 = 10
+            a = 20 * a0
+            z0 = sqrt(0.067 * m * V0 * a * a / (2 * (hbar_ev) ** 2 * Ce))
             return a, m, Ce, V0, z0, hbar_ev, hbar_Js
