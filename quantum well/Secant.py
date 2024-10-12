cd4ea3b71678103aa5a7a78bc2faada126c87a48
nguyendz21012003 <66507352+nguyn21012003@users.noreply.github.com>
2024-10-12 23:07:04 +0700

    fix Secant method

--- quantum well/Secant.py
+++ quantum well/Secant.py
@@ -1,29 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct  7 18:47:55 2024
-
-@author: hoann
-"""
 import math
 import numpy
-from numpy import pi
 
 
-def secant(f, p0, p1, e, nmax, z0):
+def secant(f, p0, p1, eps, N, z0):
     i = 2
-    p = []
-    while i < nmax:
+    while i < N:
         f_p0 = f(p0, z0)
         f_p1 = f(p1, z0)
 
-        # Công thức Secant
         p2 = p1 - f_p1 * (p1 - p0) / float(f_p1 - f_p0)
 
-        if abs(f_p1) < e:  # Nếu giá trị gần nghiệm
+        if abs(f_p1) < eps:
             break
 
-        # Kiểm tra điều kiện dừng dựa trên sai số
-        if abs(p2 - p1) < e:
+        if abs(p2 - p1) < eps:
             break
 
         p0, p1 = p1, p2
