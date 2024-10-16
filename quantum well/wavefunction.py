085e9e63ac12e2e5e1fbbb5b427f948e54856d7e
nguyendz21012003 <66507352+nguyn21012003@users.noreply.github.com>
2024-10-16 19:42:38 +0700

    update wavefunction.py

--- quantum well/wavefunction.py
+++ quantum well/wavefunction.py
@@ -3,13 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plotWaveFunction(z, a, V0, N, hbar_Js, m, Ce):
+def plotWaveFunction(z, z0, a, V0, N, hbar_Js, m, Ce):
 
     x = np.linspace(-2 * a, 2 * a, N)
-
-    x1 = np.linspace(-2 * a, 2 * a, N)
-    x2 = np.linspace(a, 2 * a, N)
-    x3 = np.linspace(-a, -2 * a, N)
 
     kappa = []
     l = []
@@ -18,24 +14,22 @@ def plotWaveFunction(z, a, V0, N, hbar_Js, m, Ce):
     for i in range(len(z)):
         l.append(z[i] / a)  # công thức 2.155 slide
         kappa.append(z[i] / a * tan(z[i]))  # công thức 2.156 slide
-        energy.append(-(20 * (hbar_Js) ** 2 * z[i] ** 2) / (m * a**2) / Ce)  # công thức 2.173 Griffiths
+        energy.append(((hbar_Js * z[i]) ** 2) / (2 * m * a**2) / Ce)  # công thức 2.173 Griffiths
 
     def V(x, V0, a):
         Vp = np.zeros(len(x))
         for i in range(len(x)):
-            if -a <= x[i] <= a:
-                Vp[i] = -V0
-            else:
-                Vp[i] = 0
+            if x[i] <= -a or x[i] >= a:
+                Vp[i] = V0
+
         return Vp
 
     def Ψ(x, V0, l, kappa, a):
 
-        D1 = V0 / 20
-        D = sqrt(1 / (a + 1 / kappa))
+        D = V0 / 60
+        # D1 = sqrt(1 / (a + 1 / kappa))
 
         F = D * (cos(l * a)) / exp(-kappa * a)
-        print(D)
 
         wave = np.zeros(len(x))
 
@@ -48,18 +42,21 @@ def plotWaveFunction(z, a, V0, N, hbar_Js, m, Ce):
                 wave[i] = D * cos(l * x[i])
         return wave
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(1, 2, figsize=(15, 7))
     U = V(x, V0, a)
-    ax.plot(x, U)
-    print(Ψ(x, V0, l[i], kappa[i], a))
+    ax[1].plot(x, U)
+    ax[1].grid(True)
     for i in range(len(l)):
-        ax.plot(x, energy[i] + Ψ(x, V0, l[i], kappa[i], a))
-        ax.axhline(y=0, ls="-.")
-        ax.axhline(y=energy[i], ls="--", xmin=0.1666, xmax=0.8333)
-        ax.annotate(f"E= {energy[i]:10f} eV", xy=(x[-1], energy[i]), xycoords="data")
-    # ax.set_xlabel(r"$x$", fontsize=18)
-    # ax.set_ylabel(r"$U(x)$", fontsize=18)
-    #
-    plt.grid()
-    # plt.legend()
+        ax[1].plot(x, energy[i] + Ψ(x, V0, l[i], kappa[i], a))
+        ax[1].axhline(y=energy[i], ls="--", xmin=0.2, xmax=0.8)
+        ax[1].annotate(f"E= {energy[i]:10f} eV", xy=(x[-1], energy[i]), xycoords="data")
+
+    z1 = np.arange(1e-15, 4 * np.pi, 1 / N)
+    rhs = tan(z1)
+    lhs = sqrt((z0 / z1) ** 2 - 1)
+    ax[0].plot(z1, rhs)
+    ax[0].plot(z1, lhs)
+    ax[0].set_ylim(-0.2, 1000 * z0)
+    ax[0].grid(True)
+
     plt.show()
