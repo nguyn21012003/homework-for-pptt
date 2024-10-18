f30e0e6a8496020c0f1de017db71fd3af3559ab5
nguyendz21012003 <66507352+nguyn21012003@users.noreply.github.com>
2024-10-18 20:27:15 +0700

    update quantum well

--- quantum well/fwave.py
+++ quantum well/fwave.py
@@ -3,78 +3,85 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-# def F(z):
-#    alpha = ((1/2 + sin(2*z)/(4*z))*(exp(-2*z*tan(z))/(cos(z)**2)) + exp(-2*z*tan(z))/(2*z*tan(z)))*a
-#    return sqrt(1/(2*alpha))
 
-
-# def D(z):
-#    return F(z)*exp(-z*tan(z))/cos(z)
+# Hệ số chuẩn hóa
+# ---------------------------------
 def F(z):
-    return exp(z * tan(z)) * cos(z) / sqrt((z * tan(z) + 1) / (z * tan(z) / a))
+    return exp(z * tan(z)) * cos(z) / sqrt((z * tan(z) + 1) / (z * tan(z)))
 
 
 def D(z):
-    return 1 / sqrt((z * tan(z) + 1) / (z * tan(z) / a))
+    return 1 / sqrt((z * tan(z) + 1) / (z * tan(z)))
 
 
+# ---------------------------------
+
+
+# Hàm sóng trong giếng (-a < x < a)
+# ---------------------------------
+def Psi1(x):
+    wave = []
+    for i in range(len(z)):
+        d = D(z)[i]
+        Z = z[i]
+        wave.append(d * cos(Z * x))
+    return np.array(wave)
+
+
+# Hàm sóng ngoài giếng ( x > a )
+# ---------------------------------
 def Psi2B(x):
     wave = []
     for i in range(len(z)):
         f = F(z)[i]
         Z = z[i]
-        wave.append(f * exp(-Z * tan(Z) * x / a))
+        wave.append(f * exp(-Z * tan(Z) * x))
     return np.array(wave)
 
 
+# ---------------------------------
+
+
+# Hàm sóng ngoài giếng ( x < a )
+# ---------------------------------
 def Psi2A(x):
     wave = []
     for i in range(len(z)):
         f = F(z)[i]
         Z = z[i]
-        wave.append(f * exp(Z * tan(Z) * x / a))
+        wave.append(f * exp(Z * tan(Z) * x))
     return np.array(wave)
 
 
-def Psi1(x):
-    wave2 = []
-    for i in range(len(z)):
-        d = D(z)[i]
-        Z = z[i]
-        wave2.append(d * cos(Z * x / a))
-    return np.array(wave2)
+# ---------------------------------
 
 
-a = 0.529e-10
-z = np.array(
-    [
-        1.5103754495117065,
-        3.2590322977622193,
-        6.342461412835801,
-        9.462989360316994,
-        12.593270514088786,
-        15.727410465230284,
-        18.863296250475514,
-        21.999727035830396,
-    ]
-)
+# Giá trị a và z
+# ---------------------------------
+a = 1e-9
+z = np.array([1.3637840514290067, 4.054915182534266, 6.491617892999432])
+# ---------------------------------
 
 
-x1 = np.linspace(-a, a, 1000)
-x2A = np.linspace(-10 * a, -a, 1000)
-x2B = np.linspace(a, 10 * a, 1000)
-v = np.sum(Psi1(x1), axis=0)
-c = np.sum(Psi2A(x2A), axis=0)
-l = np.sum(Psi2B(x2B), axis=0)
-cc = (random.random(), random.random(), random.random())
-plt.plot(x1, v, color=cc)
-plt.plot(x2A, c, color=cc)
-plt.plot(x2B, l, color=cc)
-# for i in range(8):
-#    cc=(random.random(), random.random(), random.random())
-#    plt.plot(x1,Psi1(x1)[i],color=cc,label='z'+str(i)+' = '+str(z[i]))
-#    plt.plot(x2A,Psi2A(x2A)[i],color=cc)
-#    plt.plot(x2B,Psi2B(x2B)[i],color=cc)
+# Vẽ đồ thị
+# ---------------------------------
+# Khởi tạo x
+# -------------
+x1 = np.linspace(-1, 1, 1000)
+x2A = np.linspace(-10, -1, 1000)
+x2B = np.linspace(1, 10, 1000)
+# -------------
+
+
+for i in range(len(z)):
+    m = (random.random(), random.random(), random.random())
+    plt.plot(x1, Psi1(x1)[i], color=m, label="φ" + str(i+1) + " ,z" + str(i+1) + " = " + str(z[i]))
+    plt.plot(x2A, Psi2A(x2A)[i], color=m)
+    plt.plot(x2B, Psi2B(x2B)[i], color=m)
+plt.title("Hàm riêng")
+plt.ylabel("φ(x) ( $a^{-1/2}$)")
+plt.xlabel("x ($a$)")
 plt.grid()
-# plt.legend(loc='best')
+plt.legend(loc="best")
+plt.savefig("wavefunctions.pdf")
 plt.show()
