32bbe19c1e6aab1a9e1cadf949225d7db5a45a9e
nguyendz21012003 <66507352+nguyn21012003@users.noreply.github.com>
2024-10-30 21:05:23 +0700

    update SBE

--- SBE/main.py
+++ SBE/main.py
@@ -1,78 +1,107 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.constants import hbar
+
+# from scipy.constants import hbar
 from numpy import real as RE, imag as IM, conjugate, sqrt, log, pi, exp
+from numpy import typing as npt
 
 
-def RK4(F, t, fp, dt):
-    k1 = dt * F(t, fp)
-    k2 = dt * F(t + dt / 2, fp + k1 * dt / 2)
-    k3 = dt * F(t + dt / 2, fp + k2 * dt / 2)
-    k4 = dt * F(t + dt, fp + dt * k3)
-    fp = fp + (k1 + 2 * k2 + 2 * k3 + k4) / 6
-    return fp
+# Input variables
+a0 = 125
+N = 100
+E_max = 300  # meV
+dE = E_max / N
+E_R = 4.2
+delta_t = 25  # fs
+del0 = 30  # meV
+dt = 2  # fs
+t_max = 500  # fs
+t0 = -3 * delta_t
+hbar = 658.5  # MeV fs
+chi_0 = 0.5
+T2 = 200  # fs
+
+# evẻi won í khong
+# Y = np.zeros([2, N + 1], dtype="complex")
+# for n in range(1, N + 1):
+#    Y[0][n] = 0 + 1j * 0  # f_e +i f_h
+#    Y[1][n] = 0 + 1j * 0  # p_n
 
 
-# Def fj and fn equations
-def fp(N, icInput):
-    f_e, f_h, p_n = icInput
-    fArr = np.zeros([2, N], dtype="complex")
-    for n in range(1, N + 1):
-        fArr[0][n] = f_e + 1j * f_h
-        fArr[1][n] = p_n * n
-    return fArr
+def rk4(dF: npt.NDArray, tn: npt.NDArray, yn: npt.NDArray, h: float) -> npt.NDArray:
+    # tn í value in tSpan  array
+    k1 = dF(tn, yn)
+    k2 = dF(tn + 0.5 * h, yn + 0.5 * h * k1)
+    k3 = dF(tn + 0.5 * h, yn + 0.5 * h * k2)
+    k4 = dF(tn + h, yn + h * k3)
+    return yn + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
 
 
-# Time derivative of fj and fn (SBE equations)
-def F(N, omega_R, E_n, fp, dE, d0, T2):
-    F = np.zeros((2, N))
-    for n in range(1, N + 1):
-        F[0][N] = -2 * IM(omega_R * conjugate(fp[1][n]))
-        F[1][N] = -(1j / hbar) * (n * dE - d0 - E_n) * fp[1][n] + 1j * (1 - RE(fp[0][n]) - IM(fp[0][n])) * omega_R - (fp[1][n] / T2)
-    return F
-
-
-# Some input equations
-def g(n, n1, dE):
+def g(n, n1):
     return (1 / sqrt(n * dE)) * log(abs((sqrt(n) + sqrt(n1)) / (sqrt(n) - sqrt(n1))))
 
 
-def E_n(N, E_R, dE, g, fp):
+def E_n(g, f_e, f_h):
+    E = np.zeros(N + 1)
     for n in range(1, N + 1):
         for n1 in range(1, N + 1):
-            E_n = (sqrt(E_R) / pi) * dE * g(n, n1, dE) * (RE(fp[0][n1]) + IM(fp[0][n1]))
-    return E_n
+            if n1 != n:
+                E[n] += (sqrt(E_R) / pi) * dE * g(n, n1) * (2 * f_e[n1])
+    # f_e[n1] + f_h[n1]
+    return E
 
 
-def omega_R(N, delta_t, chi_0, t, E_R, dE):
+def omega_R(t, g, p_n):
+
+    OMEGA = np.zeros(N + 1, dtype="complex")
     for n in range(1, N + 1):
         for n1 in range(1, N + 1):
-            omega_R = (1j / hbar) * (
-                1 / 2 * (hbar * sqrt(pi) / delta_t) * chi_0 * exp(-(t**2) / delta_t**2) + (sqrt(E_R) / pi) * dE * g(n, n1, dE) * fp[1][n1]
-            )
-    return omega_R
+            if n1 != n:
+                OMEGA[n] += (1 / hbar) * (
+                    0.5 * (hbar * sqrt(pi) / delta_t) * chi_0 * exp(-(t**2) / delta_t**2) + (sqrt(E_R) / pi) * dE * g(n, n1) * p_n[n1]
+                )
+
+    return OMEGA
 
 
-def input():
-    # Input variables
-    N = 100
-    E_max = 300  # meV
-    dE = E_max / N
-    delta_t = 25  # fs
-    d0 = 30  # meV
-    dt = 2  # fs
-    t_max = 300  # fs
-    t0 = -3 * delta_t
+def dF(t, Y):
 
-    # Initial conditions
-    icInput = np.zeros(3)
-    print(icInput[0])
-    print(icInput[1])
-    print(icInput[2])
+    F = np.zeros([2, N + 1], dtype="complex")
+
+    f_e = RE(Y[0])
+    f_h = IM(Y[0])
+    p_n = Y[1]
+    E = E_n(g, f_e, f_h)
+    OMEGA = omega_R(t, g, p_n)
+    for n in range(1, N + 1):
+        F[0][n] = -2 * IM(OMEGA[n] * conjugate(p_n[n]))  # phuc = thuc -> f_h =0
+        F[1][n] = -(1j / hbar) * (n * dE - del0 - E[n]) * p_n[n] + 1j * (1 - 2 * f_e[n]) * OMEGA[n] - (p_n[n] / T2)
+
+    return F
+
+
+def solve_sys_ode(dF: npt.NDArray, h: float, rk4: npt.NDArray, N: int) -> npt.NDArray:
+    y = []
+    Y = np.zeros([2, N + 1], dtype=complex)
+    tSpan = np.linspace(t0, t_max, N)
+    print(tSpan)
+    for tn in tSpan:
+        for j in range(N):
+            Y = rk4(dF, tn, Y, h)  # lặp trong yn
+            y.append(Y)
+    return tSpan, y
 
 
 def main():
-    input()
+
+    t, solution = solve_sys_ode(dF, dt, rk4, N)
+    fe = []
+    pn = []
+    for i in range(1, N + 1):
+        fe.append(RE(solution[i][0][N]))
+        pn.append(abs(solution[i][1]) ** 2)
+    plt.plot(t, solution)
+    plt.show()
 
 
 if __name__ == "__main__":
