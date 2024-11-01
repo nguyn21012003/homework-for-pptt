1be290fed942defff5d38a3c0296ae74fd12cd48
nguyendz21012003 <66507352+nguyn21012003@users.noreply.github.com>
2024-11-01 18:55:43 +0700

    update SBE

--- SBE/main.py
+++ SBE/main.py
@@ -4,28 +4,22 @@ import matplotlib.pyplot as plt
 # from scipy.constants import hbar
 from numpy import real as RE, imag as IM, conjugate, sqrt, log, pi, exp
 from numpy import typing as npt
-
+from time import time
 
 # Input variables
-a0 = 125
-N = 100
-E_max = 300  # meV
-dE = E_max / N
+N = 50
+hbar = 658.5  # MeV fs
+chi_0 = 0.5
 E_R = 4.2
-delta_t = 25  # fs
-del0 = 30  # meV
+delta_t = 500  # fs
+delta_0 = 100  # meV
 dt = 2  # fs
 t_max = 500  # fs
 t0 = -3 * delta_t
-hbar = 658.5  # MeV fs
-chi_0 = 0.5
+e_max = 300  # meV
+delta_e = e_max / N
 T2 = 200  # fs
-
-# evẻi won í khong
-# Y = np.zeros([2, N + 1], dtype="complex")
-# for n in range(1, N + 1):
-#    Y[0][n] = 0 + 1j * 0  # f_e +i f_h
-#    Y[1][n] = 0 + 1j * 0  # p_n
+a0 = 125
 
 
 def rk4(dF: npt.NDArray, tn: npt.NDArray, yn: npt.NDArray, h: float) -> npt.NDArray:
@@ -38,69 +32,97 @@ def rk4(dF: npt.NDArray, tn: npt.NDArray, yn: npt.NDArray, h: float) -> npt.NDAr
 
 
 def g(n, n1):
-    return (1 / sqrt(n * dE)) * log(abs((sqrt(n) + sqrt(n1)) / (sqrt(n) - sqrt(n1))))
+    return (1 / sqrt(n * delta_e)) * log(abs((sqrt(n) + sqrt(n1)) / (sqrt(n) - sqrt(n1))))
 
 
-def E_n(g, f_e, f_h):
-    E = np.zeros(N + 1)
-    for n in range(1, N + 1):
-        for n1 in range(1, N + 1):
-            if n1 != n:
-                E[n] += (sqrt(E_R) / pi) * dE * g(n, n1) * (2 * f_e[n1])
-    # f_e[n1] + f_h[n1]
+def E_n(n, g, f_e, f_h):
+    E = 0
+    for n1 in range(1, N + 1):
+        if n1 != n:
+            E += (sqrt(E_R) / pi) * delta_e * g(n, n1) * (f_e[n1] + f_h[n1])
     return E
 
 
-def omega_R(t, g, p_n):
+def omega_R(n, t, g, p_n):
 
-    OMEGA = np.zeros(N + 1, dtype="complex")
-    for n in range(1, N + 1):
-        for n1 in range(1, N + 1):
-            if n1 != n:
-                OMEGA[n] += (1 / hbar) * (
-                    0.5 * (hbar * sqrt(pi) / delta_t) * chi_0 * exp(-(t**2) / delta_t**2) + (sqrt(E_R) / pi) * dE * g(n, n1) * p_n[n1]
-                )
+    sum1 = 0
+    for n1 in range(1, N + 1):
+        if n1 != n:
+            sum1 += g(n, n1) * p_n[n1]
+    OMEGA = (0.5 * (hbar * sqrt(pi) / delta_t) * chi_0 * exp(-(t**2) / delta_t**2) + (sqrt(E_R) / pi) * delta_e * sum1) / hbar
 
     return OMEGA
 
 
 def dF(t, Y):
 
-    F = np.zeros([2, N + 1], dtype="complex")
+    F = np.zeros((2, N + 1), dtype="complex")
 
     f_e = RE(Y[0])
     f_h = IM(Y[0])
     p_n = Y[1]
-    E = E_n(g, f_e, f_h)
-    OMEGA = omega_R(t, g, p_n)
+
     for n in range(1, N + 1):
-        F[0][n] = -2 * IM(OMEGA[n] * conjugate(p_n[n]))  # phuc = thuc -> f_h =0
-        F[1][n] = -(1j / hbar) * (n * dE - del0 - E[n]) * p_n[n] + 1j * (1 - 2 * f_e[n]) * OMEGA[n] - (p_n[n] / T2)
+        E = E_n(n, g, f_e, f_h)
+        OMEGA = omega_R(n, t, g, p_n)
+
+        F[0][n] = -2 * IM(OMEGA * conjugate(p_n[n])) + 1j * -2 * IM(OMEGA * conjugate(p_n[n]))
+        F[1][n] = -(1j / hbar) * (n * delta_e - delta_0 - E) * p_n[n] + 1j * (1 - f_e[n] - f_h[n]) * OMEGA - (p_n[n] / T2)
 
     return F
 
 
 def solve_sys_ode(dF: npt.NDArray, h: float, rk4: npt.NDArray, N: int) -> npt.NDArray:
-    y = []  # để lưu giá trị của Y
-    Y = np.zeros([2, N + 1], dtype=complex)
+
+    Y = np.zeros((2, N + 1), dtype="complex")
+
     tSpan = np.linspace(t0, t_max, N)
-    print(tSpan)
-    for tn in tSpan:
-        for j in range(N):
-            Y = rk4(dF, tn, Y, h)  # lặp trong yn
-            y.append(Y)
-    return tSpan, y
+
+    Polarization = np.zeros(N)
+    Energy = np.zeros(N)
+    Nt = np.zeros(N)
+    fe = []
+    p = []
+    for ti in tSpan:
+        Y = rk4(dF, ti, Y, h)
+        fe_t = []
+        p_t = []
+        for n in range(0, N):
+            fe_t.append(RE(Y[0][n]))
+            p_t.append((abs(Y[1][n])))
+            Polarization[n] = delta_e * sqrt(delta_e) * sqrt(n) * RE(Y[0][n])
+            Energy[n] = n * delta_e
+            Nt[n] += sqrt(n) * RE(Y[0][n])
+        fe.append(fe_t)
+        p.append(p_t)
+
+    return tSpan, Energy, Nt, fe, p
 
 
 def main():
+    t, Energy, Nt, fe, p = solve_sys_ode(dF, dt, rk4, N)
+    T, E = np.meshgrid(t, Energy)
+    T, P = np.meshgrid(t, Nt)
+    fe = np.array(fe)
+    p = np.array(p)
 
-    t, solution = solve_sys_ode(dF, dt, rk4, N)
-    fe = []
-    pn = []
-    for i in range(1, N + 1):
-        fe.append(RE(solution[i][0][N]))
-        pn.append(abs(solution[i][1]) ** 2)
-    plt.plot(t, solution)
+    fig = plt.figure(figsize=(16, 8))
+
+    ax1 = fig.add_subplot(121, projection="3d")
+    ax1.plot_surface(T, E, Nt, cmap="viridis")
+    ax1.set_ylabel(r"Năng lượng MeV")
+    ax1.set_title(r"Hàm phân bố $f_e$ theo thời gian và năng lượng")
+
+    ax2 = fig.add_subplot(122, projection="3d")
+    ax2.plot_surface(T, E, p, cmap="plasma")
+    ax2.set_ylabel(r"Năng lượng MeV")
+    ax2.set_title(r"Hàm phân bố $f_p$ theo thời gian và năng lượng")
+
+    # ax3 = fig.add_subplot(133, projection="3d")
+    # ax3.plot_surface(T, E, P, cmap="copper")
+
+    plt.savefig("delta20fs")
+
     plt.show()
 
 
