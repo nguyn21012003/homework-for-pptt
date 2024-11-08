6ab465bda4c42f470288415b635241d919ace21c
nguyendz21012003 <66507352+nguyn21012003@users.noreply.github.com>
2024-11-08 11:08:31 +0700

    update main have plot,write

--- buoi_7_3110/main.py
+++ buoi_7_3110/main.py
@@ -1,59 +1,114 @@
 import numpy as np
 from Jacobi import FArrMatrixJacobian
+from GaussianSeidel import FArrMatrixGauss
 from solveLoop import solveLoop
-
-
+from math import sqrt
 import matplotlib.pyplot as plt
-import numpy as np
+import csv
 
 np.set_printoptions(precision=10, linewidth=120, edgeitems=12)
 
 
-def createMatrix(subDiagonal, dim):
+def createMatrix(subDiagonal: int, dim: int):
 
-    AMatrix = np.zeros((dim, dim))
-
-    BMatrix = np.full((dim, 1), -100)
+    AMatrix = np.zeros([dim, dim])
 
     for i in range(dim):
         AMatrix[i, i] = -4
-
         if i - 1 >= 0:
             AMatrix[i, i - 1] = 1
-
         if i + 1 < dim:
             AMatrix[i, i + 1] = 1
-
         if i - subDiagonal >= 0:
             AMatrix[i, i - subDiagonal] = 1
-
         if i + subDiagonal < dim:
             AMatrix[i, i + subDiagonal] = 1
+
+    BMatrix = np.zeros(dim)
+
+    for i in range(0, int(sqrt(dim))):
+        BMatrix[i] = -100
+        # BMatrix[-i] = 100
 
     return AMatrix, BMatrix
 
 
-def plotSol(solution, x):
-    pass
+def MatrixFull(AMatrix, value):
+
+    size = AMatrix.shape[0]
+
+    AMatrixFull = np.zeros([size + 2, size + 2])
+
+    for i in range(1, size + 1):
+        for j in range(1, size + 1):
+            AMatrixFull[i, j] = AMatrix[i - 1, j - 1]
+    for i in range(0, size + 2):
+        AMatrixFull[0, i] = value
+
+    return AMatrixFull
+
+
+def plotSol(grid, vjacobi, vgauss):
+
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(121, projection="3d")
+    ax2 = fig.add_subplot(122, projection="3d")
+
+    steps = grid + 2
+
+    X = np.linspace(0, grid, steps)
+    Y = np.linspace(0, grid, steps)
+    # print(len(X))
+    # print(len(vjacobi))
+
+    X, Y = np.meshgrid(X, Y)
+    ax1.plot_wireframe(X, Y, vjacobi)
+    ax2.plot_wireframe(X, Y, vgauss)
+    plt.show()
+
+
+def saveLog(file: str, N, Jacobian, Gaussian):
+    with open(file, "w", newline="") as writefile:
+        header = [f"{"n":^4}", f"{"Jacobian":^18}", f"{"Gaussian":^18}"]
+        writer = csv.DictWriter(writefile, fieldnames=header, delimiter="|")
+        writer.writeheader()
+        for i in range(0, N + 1):
+            writer.writerow(
+                {
+                    f"{"n":^4}": f"{i:^4}",
+                    f"{"Jacobian":^18}": f"{Jacobian[i]:^18}" if i < len(Jacobian) else f"{"":<18}",
+                    f"{"Gaussian":^18}": f"{Gaussian[i]:^18}" if i < len(Gaussian) else f"{"":<18}",
+                }
+            )
 
 
 def main():
-    N = 10000
-    grid = 10
-    m, n, subDiagonal = 2, 2, 3
-    dim = m * subDiagonal + n + 1
-    eigenFunction = np.full(dim, 1)
-    AMatrix, BMatrix = createMatrix(subDiagonal, dim)
+    numberLoop = 10
+    ic = 100
+    unknowPoints = 12
+    i, j = unknowPoints, unknowPoints
+    dim = unknowPoints**2
+    file = "ElectricPotentials.txt"
 
-    print(AMatrix)
-    print(BMatrix)
-    u, step = solveLoop(AMatrix, BMatrix, dim, FArrMatrixJacobian, N, eigenFunction)
-    print(u)
+    AMatrix, BMatrix = createMatrix(unknowPoints, dim)
+    eigenFunction = np.zeros(dim)
+    print((AMatrix))
+    print((BMatrix))
+    ujacobi, loops = solveLoop(AMatrix, BMatrix, dim, FArrMatrixJacobian, numberLoop, eigenFunction)
+    ugauss, loops = solveLoop(AMatrix, BMatrix, dim, FArrMatrixGauss, numberLoop, eigenFunction)
+    # u = np.linalg.solve(AMatrix, BMatrix)
+    print(ujacobi)
 
-    v = np.zeros([m + 1, n + 1])
-    v = np.reshape(u, (m + 1, n + 1))
+    vjacobi = np.reshape(ujacobi, [i, j])
+    vgauss = np.reshape(ugauss, [i, j])
 
-    print(v)
+    vjacobi = MatrixFull(vjacobi, ic)
+    vgauss = MatrixFull(vgauss, ic)
+    print(vjacobi)
+
+    plotSol(unknowPoints, vjacobi, vgauss)
+    saveLog(file, numberLoop, ujacobi, ugauss)
 
 
 if __name__ == "__main__":
