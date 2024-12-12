0acc909ae633b7547ea33317ef6dce41569b4ba0
Jeremy Tran <nguyendeptrai980@gmail.com>
2024-12-12 23:52:40 +0700

    create buoi 1

---
+++ thay Lan/buoi_1_1212/baitap2.py
@@ -0,0 +1,51 @@
+import numpy as np
+from numpy.random import rand
+from numpy import abs
+import matplotlib.pylab as plt
+
+
+def lostFucntion(N, x, y):
+    L = 1 / (2 * N) * abs(y - x)
+    return L
+
+
+def grad(N, x, y, w):
+    gradL = 1 / N * x.T.dot(x.dot(w) - y)
+    return gradL
+
+
+def Solution(N, eps, gamma, x, y):
+
+    w1 = np.zeros(1, N)
+    vt1 = 0
+    w = 0
+    b = np.dot(x.T, y)
+    A = np.dot(x.T, x)
+    for i in range(0, N):
+        vt1 = grad(N, x, y, w)
+        w1.append(float(w1[i] - vt1))
+
+        if abs(w1[i] - w1[i - 1]) <= eps:
+
+            it = i
+            break
+    return w1
+
+
+def generator(N):
+    x = rand(N, 1)
+    y = 3 + 4 * x + 0.1 * rand(N, 1)
+
+
+def main():
+    N = 1000
+    eps = 1e-15
+    gamma = 0.9
+    x, y = generator(N)
+    Solution(N, eps, gamma, x, y)
+    plt.plot(x, y, "o", markersize="1")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ thay Lan/buoi_1_1212/main.py
@@ -0,0 +1,54 @@
+import numpy as np
+from numpy import abs
+import matplotlib.pyplot as plt
+
+
+def Fx(x):
+    return x**2 + 10 * np.sin(x)
+
+
+def g(gamma, eta, vt, x):
+    x = x - vt
+
+    vt = gamma * vt + eta * (2 * x + 10 * np.cos(x))
+    return vt
+
+
+def Solution(N, eps, gamma):
+
+    eta = 0.1
+    x1 = [6]
+    x2 = [-6]
+
+    it = 0
+    vt1 = 0
+    vt2 = 0
+    for i in range(0, N):
+        vt1 = g(gamma, eta, vt1, x1[i])
+        vt2 = g(gamma, eta, vt2, x2[i])
+        x1.append(float(x1[i] - vt1))
+        x2.append(float(x2[i] - vt2))
+
+        if abs(x1[i] - x1[i - 1]) <= eps:
+            it = i
+            print(i)
+            break
+    print(x1, "\n")
+    print(x2)
+    return x1, x2
+
+
+def main():
+    N = 1000
+    eps = 1e-15
+    gamma = 0.9
+    x1, x2 = Solution(N, eps, gamma)
+    for i in range(len(x1)):
+
+        plt.plot(x1[i], Fx(x1[i]), c="purple")
+        # plt.plot(x2[i], Fx(x2[i]))
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
